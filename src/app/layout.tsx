151c3d9174c4076c201870218dce56df53053dbd
Aditya Choudhary <149503228+AdityaChoudhary01@users.noreply.github.com>
2025-08-03 22:24:09 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -84,6 +84,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="google-site-verification" content="YX_PQiZx-77ddG15mAgILKKK1bfUrvCMgc4zbSd0Hgs" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
