e63e61fa958d556dc77c41b37a069d1777e31e85
Aditya Choudhary <149503228+AdityaChoudhary01@users.noreply.github.com>
2025-08-04 20:37:01 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -93,7 +93,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
-        <meta name="theme-color" content="#09011E" />
+        <meta name="theme-color" content="#0A0114" />
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
       </head>
       <body className="font-body antialiased flex flex-col min-h-screen dark">
